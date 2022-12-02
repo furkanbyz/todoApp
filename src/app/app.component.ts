6a0e611c9ec9a8dc59232e001c7dc33e502bbd87
Furkan Beyaz <furkan_1582@hotmail.com>
2022-12-02 16:17:29 +0300

    committed
    committed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoApp';
+  items = [
+    {description:"kahvaltı",action:"no"},
+    {description:"sinema",action:"no"},
+    {description:"ders çalışma",action:"no"},
+    {description:"piknik",action:"no"}
+  ]
 }
