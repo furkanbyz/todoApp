3e6d11b4f78fa865c66be65730261ea975c1aaff
Furkan Beyaz <furkan_1582@hotmail.com>
2022-12-05 11:31:59 +0300

    Components Commit
    Components Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    TodoComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [TodoComponent]
 })
 export class AppModule { }
